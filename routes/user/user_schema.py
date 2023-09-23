bd01b9b0c00d4fba48f313f16de8508158c98a2e
modory-code <modory.code@gmail.com>
2023-09-24 01:25:36 +0900

    fix: id field 제거 및 validator not추가 (#10)

--- routes/user/user_schema.py
+++ routes/user/user_schema.py
@@ -7,7 +7,6 @@ class RoleEnum(str, Enum):
 
 # 회원 가입 스키마
 class UserCreate(BaseModel):
-    id: int
     username: str
     password1: str
     password2: str
@@ -24,13 +23,13 @@ class UserCreate(BaseModel):
     def validate_password(cls, v):
         if len(v) < 8:
             raise ValueError('비밀번호는 최소 8자리 이상이어야 합니다.')
-        if any(char.isupper() for char in v):
+        if not any(char.isupper() for char in v):
             raise ValueError('비밀번호는 대문자를 포함해야 합니다.')
-        if any(char.islower() for char in v):
+        if not any(char.islower() for char in v):
             raise ValueError('비밀번호는 소문자를 포함해야 합니다.')
-        if any(char.isdigit() for char in v):
+        if not any(char.isdigit() for char in v):
             raise ValueError('비밀번호는 숫자를 포함해야 합니다.')
-        if any(char in '!@#$%^&*' for char in v):
+        if not any(char in '!@#$%^&*' for char in v):
             raise ValueError('비밀번호는 특수문자(!@#$%^&*) 중 하나 이상을 포함해야 합니다.')
         return v
     @validator('password2')
